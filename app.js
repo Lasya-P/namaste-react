08d305ddeec9f2201c432cdfaee3032771f951ee
Lasya Puppala <lasyapuppala99@gmail.com>
2023-09-05 01:00:11 +0530

    lecture1

---
+++ app.js
@@ -0,0 +1,9 @@
+const parent = React.createElement("div",{id:"parent"},[
+React.createElement("div",{id:"child1"},[React.createElement("h1",{},"Hello World from React1!"),
+                                        React.createElement("h2",{},"Hello World from React2!")]
+                                        ),
+React.createElement("div",{id:"child2"},[React.createElement("h1",{},"Hello World from React1!"),
+                                        React.createElement("h2",{},"Hello World from React2!")])
+                                    ]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
